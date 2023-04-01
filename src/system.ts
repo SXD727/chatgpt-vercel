71cd1566e99be044d1aa75fa88d0af1d5b799a89
SXD727 <124904268+SXD727@users.noreply.github.com>
2023-04-01 21:07:04 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -14,7 +14,7 @@ export const defaultMessage = `Powered by OpenAI Vercel
 - 本站点仅作为项目演示，不提供长期服务，请在左下角输入网站密码
 - 关注公众号:宋学东, 获取最新密码。http://c.nxw.so/c6xjT
 - API Key由我自己提供,注意不要滥用。
-- 博客 www.tss-r.com
+- 博客 tss-r.com
 - 电脑访问体验更佳
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
